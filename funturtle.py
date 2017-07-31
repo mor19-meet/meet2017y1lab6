43c5216800b8ec358dea3aa69e31c95dbd2b1532
mor19-meet <mor19@meet.mit.edu>
2017-07-31 14:42:50 +0200

    Stopping here

---
+++ funturtle.py
@@ -0,0 +1,145 @@
+Python 3.5.2 (default, Nov 17 2016, 17:05:23) 
+[GCC 5.4.0 20160609] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+Traceback (most recent call last):
+  File "/home/meet/mor19_lab6/meet2017y1lab6/funturtle.py", line 3, in <module>
+    turtle.shape(square)
+NameError: name 'square' is not defined
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+Traceback (most recent call last):
+  File "/home/meet/mor19_lab6/meet2017y1lab6/funturtle.py", line 5, in <module>
+    turtle.maunloop()
+AttributeError: module 'turtle' has no attribute 'maunloop'
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+Traceback (most recent call last):
+  File "/home/meet/mor19_lab6/meet2017y1lab6/funturtle.py", line 5, in <module>
+    turtle.maunloop()
+AttributeError: module 'turtle' has no attribute 'maunloop'
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+Traceback (most recent call last):
+  File "/home/meet/mor19_lab6/meet2017y1lab6/funturtle.py", line 8, in <module>
+    turtle.maunloop()
+AttributeError: module 'turtle' has no attribute 'maunloop'
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+Traceback (most recent call last):
+  File "/home/meet/mor19_lab6/meet2017y1lab6/funturtle.py", line 9, in <module>
+    turtle.maunloop()
+AttributeError: module 'turtle' has no attribute 'maunloop'
+ 
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+Traceback (most recent call last):
+  File "/home/meet/mor19_lab6/meet2017y1lab6/funturtle.py", line 10, in <module>
+    turtle.maunloop()
+AttributeError: module 'turtle' has no attribute 'maunloop'
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+Traceback (most recent call last):
+  File "/home/meet/mor19_lab6/meet2017y1lab6/funturtle.py", line 10, in <module>
+    turtle.maunloop()
+AttributeError: module 'turtle' has no attribute 'maunloop'
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+Traceback (most recent call last):
+  File "/home/meet/mor19_lab6/meet2017y1lab6/funturtle.py", line 14, in <module>
+    square.gpto(100,100)
+AttributeError: 'Turtle' object has no attribute 'gpto'
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+Traceback (most recent call last):
+  File "/home/meet/mor19_lab6/meet2017y1lab6/funturtle.py", line 22, in <module>
+    tryangle.shape('tryangle')
+  File "/usr/lib/python3.5/turtle.py", line 2776, in shape
+    raise TurtleGraphicsError("There is no shape named %s" % name)
+turtle.TurtleGraphicsError: There is no shape named tryangle
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+Traceback (most recent call last):
+  File "/home/meet/mor19_lab6/meet2017y1lab6/funturtle.py", line 22, in <module>
+    tryangle.shape('tryangle')
+  File "/usr/lib/python3.5/turtle.py", line 2776, in shape
+    raise TurtleGraphicsError("There is no shape named %s" % name)
+turtle.TurtleGraphicsError: There is no shape named tryangle
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+Traceback (most recent call last):
+  File "/home/meet/mor19_lab6/meet2017y1lab6/funturtle.py", line 22, in <module>
+    tryangle.shape('tryangle')
+  File "/usr/lib/python3.5/turtle.py", line 2776, in shape
+    raise TurtleGraphicsError("There is no shape named %s" % name)
+turtle.TurtleGraphicsError: There is no shape named tryangle
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+Traceback (most recent call last):
+  File "/home/meet/mor19_lab6/meet2017y1lab6/funturtle.py", line 22, in <module>
+    tryangle.shape('tryangle')
+  File "/usr/lib/python3.5/turtle.py", line 2776, in shape
+    raise TurtleGraphicsError("There is no shape named %s" % name)
+turtle.TurtleGraphicsError: There is no shape named tryangle
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+Traceback (most recent call last):
+  File "/home/meet/mor19_lab6/meet2017y1lab6/funturtle.py", line 22, in <module>
+    tryangle.shape('tryangle')
+  File "/usr/lib/python3.5/turtle.py", line 2776, in shape
+    raise TurtleGraphicsError("There is no shape named %s" % name)
+turtle.TurtleGraphicsError: There is no shape named tryangle
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+Traceback (most recent call last):
+  File "/home/meet/mor19_lab6/meet2017y1lab6/funturtle.py", line 24, in <module>
+    tryangle.shape('tryangle')
+  File "/usr/lib/python3.5/turtle.py", line 2776, in shape
+    raise TurtleGraphicsError("There is no shape named %s" % name)
+turtle.TurtleGraphicsError: There is no shape named tryangle
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+Traceback (most recent call last):
+  File "/home/meet/mor19_lab6/meet2017y1lab6/funturtle.py", line 24, in <module>
+    triangle.shape('tryangle')
+  File "/usr/lib/python3.5/turtle.py", line 2776, in shape
+    raise TurtleGraphicsError("There is no shape named %s" % name)
+turtle.TurtleGraphicsError: There is no shape named tryangle
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+Traceback (most recent call last):
+  File "/home/meet/mor19_lab6/meet2017y1lab6/funturtle.py", line 30, in <module>
+    triangle.stame()
+AttributeError: 'Turtle' object has no attribute 'stame'
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+>>> 
+========= RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle.py =========
+>>> 
+======== RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle1.py ========
+Traceback (most recent call last):
+  File "/home/meet/mor19_lab6/meet2017y1lab6/funturtle1.py", line 3, in <module>
+    turtle.shapr('turtle')
+AttributeError: module 'turtle' has no attribute 'shapr'
+>>> 
+======== RESTART: /home/meet/mor19_lab6/meet2017y1lab6/funturtle1.py ========
+>>> 
---
+++ funturtle1.py
@@ -0,0 +1,19 @@
+import turtle
+
+turtle.shape('turtle')
+square=turtle.clone()
+square.shape('square')
+square.goto(100,100)
+square.goto(300,300)
+square.stamp()
+square.goto(100,100)
+
+turtle.mainloop()
+
+up_arrow='up'
+left_arrow='left'
+down_arrow='down
+right_arrow-'right'
+spacebar='space'
+up=0
+
